b10e65cf7071f0a9faa7df36f0621aeccd32791d
Jean-Philippe MATZ <jeanphilippe.matz@gmail.com>
2024-01-29 10:47:00 +0100

    export mocks

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 export { createReactHassCard, useLovelaceCard } from "./createReactHassCard";
 export type { HassCardProps, HassCardConfig } from "./createReactHassCard";
 
+export { mockReactCardProps } from "../utils/mock";
+
 export type {
   HomeAssistant,
   LovelaceCard,
