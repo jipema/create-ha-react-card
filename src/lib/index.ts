87a132fe90f81e9b9125372c67c9edd28d018cff
Jean-Philippe MATZ <jeanphilippe.matz@gmail.com>
2024-01-29 10:33:36 +0100

    improve types export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 export { createReactHassCard, useLovelaceCard } from "./createReactHassCard";
+export type { HassCardProps, HassCardConfig } from "./createReactHassCard";
+export type {
+  HomeAssistant,
+  LovelaceCard,
+  LovelaceCardConfig,
+} from "custom-card-helpers";
