3e642b62d742d4ecb5857a3862f793c32897195e
Jean-Philippe MATZ <jeanphilippe.matz@gmail.com>
2024-02-01 21:00:53 +0100

    fix imports for tsc build

--- src/lib/CardDialog.tsx
+++ src/lib/CardDialog.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import React, { useCallback, useRef } from "react";
 
 export type CardDialogProps = React.PropsWithChildren & {
   title?: React.ReactNode | false;
