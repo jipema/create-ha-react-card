09aea9279d858b7e56a7ad22ca5a68066861b8da
Jean-Philippe MATZ <jeanphilippe.matz@gmail.com>
2024-03-05 23:54:57 +0100

    cleanup

--- src/utils/misc.d.ts
+++
@@ -1,8 +0,0 @@
-import { Panel } from "custom-card-helpers";
-export interface Omit {
-    <T extends object, K extends [...(keyof T)[]]>(obj: T, ...keys: K): {
-        [K2 in Exclude<keyof T, K[number]>]: T[K2];
-    };
-}
-export declare const omit: Omit;
-export declare function isPanelValue(value: unknown): value is Panel;
